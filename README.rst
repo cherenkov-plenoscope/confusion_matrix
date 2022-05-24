74672db14a2a62f9668ebffe82e1616c33745e36
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-05-24 15:57:12 +0200

    init

---
+++ README.rst
@@ -0,0 +1,12 @@
+Create Confusion Matrices
+=========================
+|BlackStyle|
+
+Make and organize your confusion-matrices.
+
+- Histogramming
+- Estimator for uncertainty based on count-statistics.
+- Naming of axes to not confuse the axes.
+
+.. |BlackStyle| image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="confusion_matrix_sebastian-achim-mueller",
+    version="0.0.0",
+    description="Create confusion-matrices",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/cherenkov-plenoscope/confusion_matrix",
+    project_urls={
+        "Bug Tracker": "https://github.com/cherenkov-plenoscope/confusion_matrix/issues",
+    },
+    author="Sebastian Achim Mueller",
+    author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
+    packages=["confusion_matrix",],
+    package_data={"confusion_matrix": [os.path.join("tests", "resources", "*")]},
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+    ],
+)
